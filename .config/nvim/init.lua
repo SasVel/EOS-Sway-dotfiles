6816c1a1edab49ebb846433c19671d69127dcb01
Sas <sasvelchev@gmail.com>
2025-09-19 15:13:05 +0300

    Cleanup and strudel.nvim added to nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -322,6 +322,14 @@ require("lazy").setup({
 		lazy = false,
 		version = "*", -- or branch = "main", to use the latest commit
 	},
+	{
+		"gruvw/strudel.nvim",
+		build = "npm install",
+		config = function()
+			require("strudel").setup()
+		end,
+		lazy = false,
+	},
 	{ -- Useful plugin to show you pending keybinds.
 		"folke/which-key.nvim",
 		event = "VimEnter", -- Sets the loading event to 'VimEnter'
@@ -1067,7 +1075,15 @@ require("lazy").setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+local strudel = require("strudel")
 
+vim.keymap.set("n", "<leader>sl", strudel.launch, { desc = "Launch Strudel" })
+vim.keymap.set("n", "<leader>sq", strudel.quit, { desc = "Quit Strudel" })
+vim.keymap.set("n", "<leader>st", strudel.toggle, { desc = "Strudel Toggle Play/Stop" })
+vim.keymap.set("n", "<leader>su", strudel.update, { desc = "Strudel Update" })
+vim.keymap.set("n", "<leader>ss", strudel.stop, { desc = "Strudel Stop Playback" })
+vim.keymap.set("n", "<leader>sb", strudel.set_buffer, { desc = "Strudel set current buffer" })
+vim.keymap.set("n", "<leader>sx", strudel.execute, { desc = "Strudel set current buffer and update" })
 -- Basic settings
 vim.cmd([[
   highlight Normal guibg=none
