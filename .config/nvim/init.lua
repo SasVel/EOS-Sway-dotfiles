37a0ff4ad3ac03b725b88482a37f37e34a7267ca
Sas <sasvelchev@gmail.com>
2025-09-20 12:11:24 +0300

    Disable autoformat custom command

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -809,7 +809,11 @@ require("lazy").setup({
 				-- have a well standardized coding style. You can add additional
 				-- languages here or re-enable it for the disabled ones.
 				local disable_filetypes = { c = true, cpp = true }
-				if disable_filetypes[vim.bo[bufnr].filetype] then
+				if
+					vim.g.disable_autoformat
+					or vim.b[bufnr].disable_autoformat
+					or disable_filetypes[vim.bo[bufnr].filetype]
+				then
 					return nil
 				else
 					return {
@@ -1103,3 +1107,21 @@ vim.cmd([[
 vim.cmd("syntax enable")
 vim.cmd("Screenkey toggle")
 print("Drink water!")
+
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+	if args.bang then
+		-- FormatDisable! will disable formatting just for this buffer
+		vim.b.disable_autoformat = true
+	else
+		vim.g.disable_autoformat = true
+	end
+end, {
+	desc = "Disable autoformat-on-save",
+	bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+	vim.b.disable_autoformat = false
+	vim.g.disable_autoformat = false
+end, {
+	desc = "Re-enable autoformat-on-save",
+})
