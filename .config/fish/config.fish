e3e4d458ac978b6fb223746fdb8e35724d59531e
SasVel <sasvelchev@gmail.com>
2025-09-21 11:11:07 +0300

    Added my fonts, update to btop, set nvim as default editor in fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 	alias update "sudo pacman -Syu"
 	alias godot "$HOME/Programs/GameEngines/Godot_v4.5-stable_linux.x86_64"
+	
+	set -gx EDITOR nvim
 
 	source "$HOME/dotfiles/.bashrc_private"
 end
