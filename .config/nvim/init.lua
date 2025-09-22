09c494e22bc9f2ffba11a7cb820515a67fd97e0d
Sas <sasvelchev@gmail.com>
2025-09-22 22:16:10 +0300

    new lsp syntax deprecation

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -712,7 +712,7 @@ require("lazy").setup({
 			--  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
 			--  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
 			local capabilities = require("blink.cmp").get_lsp_capabilities()
-			require("lspconfig").gdscript.setup(capabilities)
+			vim.lsp.config("gdscript", capabilities)
 			-- Enable the following language servers
 			--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
 			--
@@ -781,7 +781,7 @@ require("lazy").setup({
 						-- by the server configuration above. Useful when disabling
 						-- certain features of an LSP (for example, turning off formatting for ts_ls)
 						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-						require("lspconfig")[server_name].setup(server)
+						vim.lsp.config(server_name, server)
 					end,
 				},
 			})
